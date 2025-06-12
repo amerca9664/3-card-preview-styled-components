2a0d5b5957014f1d83ea6db206018c7a095cd706
amerca9664 <amerca9664@gmail.com>
2025-06-12 14:27:11 -0500

    added prop key

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
+import { v4 } from 'uuid';
 import Card from './components/Card/Card';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { COLORS } from './styles/colors';
+
+const cards = [
+	{
+		colorObj: COLORS.Bright_orange,
+		titleObj: 'SEDANS',
+		imageObj: 'icon-sedans.svg',
+		textObj:
+			'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, minus, perferendis deserunt voluptate praesentium fugiat et veniam nam error officiis est sunt optio eveniet rerum. Perspiciatis dolores officiis dolor doloremque!'
+	},
+	{
+		colorObj: COLORS.Dark_cyan,
+		titleObj: 'SUVS',
+		imageObj: 'icon-suvs.svg',
+		textObj:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi odio natus molestiae ipsum quisquam perspiciatis nam tempora, quos voluptates, commodi pariatur reprehenderit? Suscipit eaque perferendis dolorum omnis molestias, magni autem?'
+	},
+	{
+		colorObj: COLORS.Very_dark_cyan,
+		titleObj: 'LUXURY',
+		imageObj: 'icon-luxury.svg',
+		textObj:
+			' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, voluptates adipisci voluptas, perferendis, odio vitae aliquid blanditiis delectus tenetur quas animi beatae neque nobis deserunt quo suscipit sed distinctio accusantium!'
+	}
+];
+
+const cardInfo = iteam => (
+	<Card
+		key={v4()}
+		color={iteam.colorObj}
+		title={iteam.titleObj}
+		image={iteam.imageObj}
+	>
+		{iteam.textObj}
+	</Card>
+);
 
 const App = () => {
 	return (
 		<div>
 			<GlobalStyles />
 			<h1>Curso de React</h1>
-			<div className='contenedorCards'>
-				<Card
-					color={COLORS.Bright_orange}
-					title='SEDANS'
-					image='icon-sedans.svg'
-				>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione,
-					minus, perferendis deserunt voluptate praesentium fugiat et veniam nam
-					error officiis est sunt optio eveniet rerum. Perspiciatis dolores
-					officiis dolor doloremque!
-				</Card>
-				<Card color={COLORS.Dark_cyan} title='SUVS' image='icon-suvs.svg'>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi odio
-					natus molestiae ipsum quisquam perspiciatis nam tempora, quos
-					voluptates, commodi pariatur reprehenderit? Suscipit eaque perferendis
-					dolorum omnis molestias, magni autem?
-				</Card>
-				<Card
-					color={COLORS.Very_dark_cyan}
-					title='LUXURY'
-					image='icon-luxury.svg'
-				>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum,
-					voluptates adipisci voluptas, perferendis, odio vitae aliquid
-					blanditiis delectus tenetur quas animi beatae neque nobis deserunt quo
-					suscipit sed distinctio accusantium!
-				</Card>
-			</div>
+			<div className='contenedorCards'>{cards.map(cardInfo)}</div>
 		</div>
 	);
 };
